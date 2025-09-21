d868dda8b791bf770b5b2e71cd78705c42da94c8
flogit2161 <floj.jourdain@gmail.com>
2025-09-21 10:50:30 +0200

    verbose flag added

--- main.py
+++ main.py
@@ -2,21 +2,40 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-client = genai.Client(api_key=api_key)
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
 
-if len(sys.argv) < 2 :
-    print('wrong usage, please try uv run main.py "prompt"')
-    sys.exit(1)
-prompt = sys.argv[1]
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=prompt
-)
+    if len(sys.argv) < 2 :
+        print('wrong usage, please try uv run main.py "prompt"')
+        sys.exit(1)
+    
+    
+    prompt = sys.argv[1]
+    messages = [
+        types.Content(role='user', parts=[types.Part(text=prompt)])
+    ]
+    verbose = '--verbose' in sys.argv[2:]
+    generate_content(client, messages, verbose)
 
-print(response.text)
-tokens = response.usage_metadata.prompt_token_count
-candidates = response.usage_metadata.candidates_token_count
-print(f'Prompt tokens: {tokens}')
-print(f'Response tokens: {candidates}')
+
+
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents=messages,
+    )
+
+    print(response.text)
+    user_prompt = sys.argv[1]
+    tokens = response.usage_metadata.prompt_token_count
+    candidates = response.usage_metadata.candidates_token_count
+    if verbose:
+        print(f'User prompt: {user_prompt}')
+        print(f'Prompt tokens: {tokens}')
+        print(f'Response tokens: {candidates}')
+
+if __name__ == '__main__':
+    main()
